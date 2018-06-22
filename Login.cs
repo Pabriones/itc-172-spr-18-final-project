cf833af44b5f76970a85f901e1c407fb54e85249
Pabriones <35351141+Pabriones@users.noreply.github.com>
2018-06-22 00:10:15 -0700

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final_Project.Models
+{
+    public class Login
+        {
+            public Login() { }
+            public Login(string user, string password)
+            {
+                UserName = user;
+                PassWord = password;
+            }
+            public string UserName { set; get; }
+            public string PassWord { set; get; }
+        }
+}
